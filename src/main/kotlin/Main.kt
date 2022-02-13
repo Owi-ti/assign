76f402d678e84793335cac2eac413b37ce6cbc84
Munde-J <mundeangelah@gmail.com>
2022-02-13 20:09:27 +0300

    first kotlin project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+        var name: String = "Esther Awino Owiti"
+        var age: Byte= 20
+        var phoneNumber: String= "0114618145"
+        var weight: Int= 55
+        var studentKenyancitizen: Boolean= true
+        var studentnotKenyancitizen: Boolean= false
+
+        println(name)
+        println(age)
+        println(phoneNumber)
+        println(weight)
+        println(studentKenyancitizen)
+        println(studentnotKenyancitizen)
+
+
+}
